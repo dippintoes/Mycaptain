50fafddf019089e1ad2f55983a2f096b2369195f
jainrutu@523 <72350384+dippintoes@users.noreply.github.com>
2020-10-06 12:35:32 +0530

    Learning C++
    First file consists of program of SIZEOF function and second file consists of simple multiplication program

---
+++ mycap1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    char ch;
+    float f;
+    double d;
+    cout<<"Size of char  : "<<sizeof(ch)<<" byte"<<"\n";
+    cout<<"Size of int   : "<<sizeof(i)<<" bytes" <<"\n";
+    cout<<"Size of float : "<<sizeof(f)<<" bytes"<<"\n";
+    cout<<"Size of double: "<<sizeof(d)<<" bytes";
+}
---
+++ mycap2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cout<<"Enter two numbers"<<"\n";
+    cin>>a>>b;
+    cout<<"Product = "<<a*b;
+    return 0;
+}
